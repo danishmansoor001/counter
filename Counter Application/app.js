855421a6055df8a7d0aafde12d1e264a2bea2a37
Danish Mansoor <157880934+danishmansoor001@users.noreply.github.com>
2024-05-07 16:04:51 +0500

    Add files via upload

---
+++ Counter Application/app.js
@@ -0,0 +1,10 @@
+let countervalue=0
+function min(){
+    countervalue--
+    document.getElementById('counter').innerText=countervalue
+
+}
+function max(){
+    countervalue++
+    document.getElementById('counter').innerText=countervalue
+}
